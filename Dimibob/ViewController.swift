ee482961e8c81dbbb1e37c91738d4cd60dd21bb3
MR.LEE(leeshoon1344) <dsa28s@naver.com>
2017-03-16 13:59:33 +0900

    Initial Commit 2

---
+++ Dimibob/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dimibob
+//
+//  Created by 상훈 on 2017. 3. 16..
+//  Copyright © 2017년 MR.LEE(leeshoon1344). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
